9ff0cdb70bf0165d7455651d8c2b894cc093829f
Keagan Hockly <keagan.hockly@kingsway.school.nz>
2026-05-22 14:20:21 +1200

    Add files via upload
    This is my first program.

---
+++ hello_world.py
@@ -0,0 +1,20 @@
+greeting = "Hello World"
+print(greeting)
+
+say_hello = "hello"
+
+# Different types of strings 
+string_example1 = "This is a string." # A string contains speechmarks/Inverted commas.
+string_example2 = "7"
+
+# Addidtion Example.
+number_1 = "7"
+number_2 = "5"
+
+sum = number_1 + number_2
+print(sum)
+
+# coffee program. Checking whether you like coffee
+like_coffee = input ("Do you like coffee?")
+
+
